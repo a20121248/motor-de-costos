24e2e52c9052e848265705fd304d1929b7bcabe4
Briggette Román <briggette.roman@gmail.com>
2019-10-17 18:51:20 -0500

    Copiado de parametria.

--- src/dao/ParametrizacionDAO.java
+++ src/dao/ParametrizacionDAO.java
@@ -12,7 +12,7 @@ public class ParametrizacionDAO {
         this.connection = new ConnectionDB();
     }
         
-    public void copiarCuentasContables(int periodoOrigen, int periodoDestino, String fechaStr, int repartoTipo) {
+    public void copiarCuentasContables(int periodoOrigen, int periodoDestino, int repartoTipo) {
         String queryStr = String.format("" +
                 "DELETE FROM MS_plan_de_cuenta_lineas\n" +
                 " WHERE periodo=%d AND reparto_tipo = %d",
@@ -30,7 +30,7 @@ public class ParametrizacionDAO {
                     repartoTipo);
         ConexionBD.ejecutar(queryStr);
     }
-    public void copiarPartidas(int periodoOrigen, int periodoDestino, String fechaStr, int repartoTipo) {
+    public void copiarPartidas(int periodoOrigen, int periodoDestino, int repartoTipo) {
         String queryStr = String.format("" +
                 "DELETE FROM MS_partida_lineas\n" +
                 " WHERE periodo=%d AND reparto_tipo=%d",
@@ -49,7 +49,7 @@ public class ParametrizacionDAO {
         ConexionBD.ejecutar(queryStr);
     }
     
-    public void copiarCuentaPartida(int periodoOrigen, int periodoDestino, String fechaStr, int repartoTipo) {
+    public void copiarCuentaPartida(int periodoOrigen, int periodoDestino, int repartoTipo) {
         String queryStr = String.format("" +
                 "DELETE FROM MS_partida_cuenta_contable\n" +
                 " WHERE periodo=%d AND reparto_tipo=%d",
@@ -68,22 +68,22 @@ public class ParametrizacionDAO {
         ConexionBD.ejecutar(queryStr);
     }
     
-    public void copiarCentroLineas(int periodoOrigen, int periodoDestino, String fechaStr) {
+    public void copiarCentro(int periodoOrigen, int periodoDestino, int repartoTipo) {
         String queryStr = String.format("" +
-                "DELETE FROM centro_lineas\n" +
-                " WHERE periodo=%d",
-                periodoDestino);
+                "DELETE FROM MS_centro_lineas\n" +
+                " WHERE periodo=%d and periodo =%d",
+                periodoDestino, repartoTipo);
         ConexionBD.ejecutar(queryStr);
 
         queryStr = String.format("" +
-                "INSERT INTO centro_lineas(centro_codigo,periodo,iteracion,saldo,grupo_gasto,fecha_creacion,fecha_actualizacion,entidad_origen_codigo)\n" +
-                "SELECT centro_codigo,%d periodo,-2 iteracion,0 saldo,'-' grupo_gasto,TO_DATE('%s','yyyy/mm/dd hh24:mi:ss') fecha_creacion,TO_DATE('%s','yyyy/mm/dd hh24:mi:ss') fecha_actualizacion,0 entidad_origen_codigo\n" +
-                "  FROM centro_lineas\n" +
-                " WHERE periodo=%d AND iteracion=-1",
+                "INSERT INTO MS_centro_lineas\n" +
+                "(centro_codigo,periodo,iteracion,saldo,entidad_origen_codigo,grupo_gasto,reparto_tipo,cuenta_contable_origen_codigo,partida_origen_codigo,centro_origen_codigo,fecha_creacion,fecha_actualizacion)\n" +
+                "SELECT centro_codigo,%d periodo,-2 iteracion,0 saldo,entidad_origen_codigo, grupo_gasto, reparto_tipo, cuenta_contable_origen_codigo, partida_origen_codigo, centro_origen_codigo,sysdate,sysdate\n" +
+                "  FROM MS_centro_lineas\n" +
+                " WHERE periodo=%d AND reparto_tipo=%d AND iteracion=-2",
                 periodoDestino,
-                fechaStr,
-                fechaStr,
-                periodoOrigen);
+                periodoOrigen,
+                repartoTipo);
         ConexionBD.ejecutar(queryStr);
     }
 
@@ -123,103 +123,176 @@ public class ParametrizacionDAO {
         ConexionBD.ejecutar(queryStr);
     }
         
-    public void copiarProductos(int periodoOrigen, int periodoDestino, String fechaStr) {
+    public void copiarProductos(int periodoOrigen, int periodoDestino, int repartoTipo) {
         String queryStr = String.format("" +
-                "DELETE FROM producto_lineas\n" +
-                " WHERE periodo=%d",
-                periodoDestino);
+                "DELETE FROM MS_producto_lineas\n" +
+                " WHERE periodo=%d and reparto_tipo=%d",
+                periodoDestino,repartoTipo);
         ConexionBD.ejecutar(queryStr);
         
         queryStr = String.format("" +
-                    "INSERT INTO producto_lineas(producto_codigo,periodo,fecha_creacion,fecha_actualizacion)\n" +
-                    "SELECT producto_codigo,%d periodo,TO_DATE('%s','yyyy/mm/dd hh24:mi:ss') fecha_creacion,TO_DATE('%s','yyyy/mm/dd hh24:mi:ss') fecha_actualizacion\n" +
-                    "  FROM producto_lineas\n" +
-                    " WHERE periodo=%d",
+                    "INSERT INTO MS_producto_lineas(producto_codigo,periodo,reparto_tipo,fecha_creacion,fecha_actualizacion)\n" +
+                    "SELECT producto_codigo,%d periodo,reparto_tipo,sysdate,sysdate\n" +
+                    "  FROM MS_producto_lineas\n" +
+                    " WHERE periodo=%d and reparto_tipo=%d",
                     periodoDestino,
-                    fechaStr,
-                    fechaStr,
-                    periodoOrigen);
-        ConexionBD.ejecutar(queryStr);
-    }
-
-    public void copiarDriverCentroLineas(int periodoOrigen, int periodoDestino, String fechaStr) {
-        String queryStr = String.format("" +
-                "DELETE FROM driver_lineas\n" +
-                " WHERE periodo=%d",
-                periodoDestino);
+                    periodoOrigen,
+                    repartoTipo);
         ConexionBD.ejecutar(queryStr);
         
         queryStr = String.format("" +
-                "INSERT INTO driver_lineas(driver_codigo,entidad_destino_codigo,periodo,porcentaje,fecha_creacion,fecha_actualizacion)\n" +
-                "SELECT driver_codigo,entidad_destino_codigo,%d periodo,porcentaje,TO_DATE('%s','yyyy/mm/dd hh24:mi:ss') fecha_creacion,TO_DATE('%s','yyyy/mm/dd hh24:mi:ss') fecha_actualizacion\n" +
-                "  FROM driver_lineas\n" +
-                " WHERE periodo=%d",
-                periodoDestino,
-                fechaStr,
-                fechaStr,
-                periodoOrigen);
-        ConexionBD.ejecutar(queryStr);
-    }
-
-    public void copiarDriverObjetoLineas(int periodoOrigen, int periodoDestino, String fechaStr) {
-        String queryStr = String.format("" +
-                "DELETE FROM driver_obco_lineas\n" +
-                " WHERE periodo=%d",
-                periodoDestino);
-        ConexionBD.ejecutar(queryStr);
-        
-        queryStr = String.format("" +
-                "INSERT INTO driver_obco_lineas(driver_codigo,oficina_codigo,banca_codigo,producto_codigo,periodo,porcentaje,fecha_creacion,fecha_actualizacion)\n" +
-                "SELECT driver_codigo,oficina_codigo,banca_codigo,producto_codigo,%d periodo,porcentaje,TO_DATE('%s','yyyy/mm/dd hh24:mi:ss') fecha_creacion,TO_DATE('%s','yyyy/mm/dd hh24:mi:ss') fecha_actualizacion\n" +
-                "  FROM driver_obco_lineas\n" +
-                " WHERE periodo=%d",
-                periodoDestino,
-                fechaStr,
-                fechaStr,
-                periodoOrigen);
+                    "INSERT INTO MS_JERARQUIA(periodo,entidad_codigo,entidad_tipo,nivel,entidad_padre_codigo,reparto_tipo)\n" +
+                    "SELECT %d periodo,entidad_codigo,entidad_tipo,nivel,entidad_padre_codigo,reparto_tipo\n" +
+                    "  FROM MS_JERARQUIA\n" +
+                    " WHERE periodo=%d and reparto_tipo=%d and entidad_tipo='PRO'",
+                    periodoDestino,
+                    periodoOrigen,
+                    repartoTipo);
         ConexionBD.ejecutar(queryStr);
     }
     
-    public void copiarDriverAsignacionEntidades(int periodoOrigen, int periodoDestino, String fechaStr) {
+    public void copiarSubcanales(int periodoOrigen, int periodoDestino, int repartoTipo) {
         String queryStr = String.format("" +
-                "DELETE FROM entidad_origen_driver\n" +
-                " WHERE periodo=%d",
-                periodoDestino);
+                "DELETE FROM MS_subcanal_lineas\n" +
+                " WHERE periodo=%d and reparto_tipo=%d",
+                periodoDestino,repartoTipo);
         ConexionBD.ejecutar(queryStr);
         
         queryStr = String.format("" +
-                "INSERT INTO entidad_origen_driver(entidad_origen_codigo,driver_codigo,periodo,fecha_creacion,fecha_actualizacion)\n" +
-                "SELECT entidad_origen_codigo,driver_codigo,%d periodo,TO_DATE('%s','yyyy/mm/dd hh24:mi:ss') fecha_creacion,TO_DATE('%s','yyyy/mm/dd hh24:mi:ss') fecha_actualizacion\n" +
-                "  FROM entidad_origen_driver\n" +
-                " WHERE periodo=%d",
+                    "INSERT INTO MS_subcanal_lineas(subcanal_codigo,periodo,reparto_tipo,fecha_creacion,fecha_actualizacion)\n" +
+                    "SELECT subcanal_codigo,%d periodo,reparto_tipo,sysdate,sysdate\n" +
+                    "  FROM MS_subcanal_lineas\n" +
+                    " WHERE periodo=%d and reparto_tipo=%d",
+                    periodoDestino,
+                    periodoOrigen,
+                    repartoTipo);
+        ConexionBD.ejecutar(queryStr);
+        
+        queryStr = String.format("" +
+                    "INSERT INTO MS_JERARQUIA(periodo,entidad_codigo,entidad_tipo,nivel,entidad_padre_codigo,reparto_tipo)\n" +
+                    "SELECT %d periodo,entidad_codigo,entidad_tipo,nivel,entidad_padre_codigo,reparto_tipo\n" +
+                    "  FROM MS_JERARQUIA\n" +
+                    " WHERE periodo=%d and reparto_tipo=%d and entidad_tipo='SCA'",
+                    periodoDestino,
+                    periodoOrigen,
+                    repartoTipo);
+        ConexionBD.ejecutar(queryStr);
+    }
+
+    public void copiarDriverCentroLineas(int periodoOrigen, int periodoDestino, int repartoTipo) {
+        String queryStr = String.format("" +
+                "DELETE FROM MS_driver_lineas\n" +
+                " WHERE periodo=%d and reparto_tipo=%d",
+                periodoDestino,repartoTipo);
+        ConexionBD.ejecutar(queryStr);
+        
+        queryStr = String.format("" +
+                "INSERT INTO MS_driver_lineas(driver_codigo,entidad_destino_codigo,periodo,porcentaje,reparto_tipo,fecha_creacion,fecha_actualizacion)\n" +
+                "SELECT driver_codigo,entidad_destino_codigo,%d periodo,porcentaje,reparto_tipo,sysdate,sysdate\n" +
+                "  FROM MS_driver_lineas\n" +
+                " WHERE periodo=%d and reparto_tipo=%d",
                 periodoDestino,
-                fechaStr,
-                fechaStr,
-                periodoOrigen);
+                periodoOrigen,
+                repartoTipo);
+        ConexionBD.ejecutar(queryStr);
+    }
+
+    public void copiarDriverObjetoLineas(int periodoOrigen, int periodoDestino, int repartoTipo) {
+        String queryStr = String.format("" +
+                "DELETE FROM ms_driver_objeto_lineas\n" +
+                " WHERE periodo=%d and reparto_tipo=%d",
+                periodoDestino,repartoTipo);
+        ConexionBD.ejecutar(queryStr);
+        
+        queryStr = String.format("" +
+                "INSERT INTO ms_driver_objeto_lineas"+
+                "(driver_codigo,producto_codigo,subcanal_codigo,periodo,porcentaje,reparto_tipo,fecha_creacion,fecha_actualizacion)\n" +
+                "SELECT driver_codigo,producto_codigo,subcanal_codigo,%d periodo,porcentaje,reparto_tipo,sysdate,sysdate\n" +
+                "  FROM ms_driver_objeto_lineas\n" +
+                " WHERE periodo=%d and reparto_tipo=%d",
+                periodoDestino,
+                periodoOrigen,
+                repartoTipo);
+        ConexionBD.ejecutar(queryStr);
+    }
+    
+    public void copiarDriverAsignacionCentroStaff(int periodoOrigen, int periodoDestino, int repartoTipo) {
+        String queryStr = String.format("" +
+                "DELETE FROM MS_entidad_origen_driver\n" +
+                " WHERE periodo=%d and reparto_tipo=%d",
+                periodoDestino,repartoTipo);
+        ConexionBD.ejecutar(queryStr);
+        
+        queryStr = String.format("" +
+                "INSERT INTO MS_entidad_origen_driver(entidad_origen_codigo,driver_codigo,periodo,reparto_tipo,fecha_creacion,fecha_actualizacion)\n" +
+                "SELECT entidad_origen_codigo,driver_codigo,%d periodo,reparto_tipo,sysdate,sysdate\n" +
+                "  FROM MS_entidad_origen_driver\n" +
+                " WHERE periodo=%d AND reparto_tipo =%d",
+                periodoDestino,
+                periodoOrigen,
+                repartoTipo);
+        ConexionBD.ejecutar(queryStr);
+    }
+    
+    public void copiarDriverAsignacionCentroBolsa(int periodoOrigen, int periodoDestino, int repartoTipo) {
+        String queryStr = String.format("" +
+                "DELETE FROM MS_bolsa_driver\n" +
+                " WHERE periodo=%d and reparto_tipo=%d",
+                periodoDestino,repartoTipo);
+        ConexionBD.ejecutar(queryStr);
+        
+        queryStr = String.format("" +
+                "INSERT INTO MS_bolsa_driver(driver_codigo,cuenta_contable_codigo,partida_codigo,centro_codigo,periodo,reparto_tipo,fecha_creacion,fecha_actualizacion)\n" +
+                "SELECT driver_codigo,cuenta_contable_codigo,partida_codigo,centro_codigo,%d periodo,reparto_tipo,sysdate,sysdate\n" +
+                "  FROM MS_bolsa_driver\n" +
+                " WHERE periodo=%d AND reparto_tipo =%d",
+                periodoDestino,
+                periodoOrigen,
+                repartoTipo);
+        ConexionBD.ejecutar(queryStr);
+    }
+    
+    public void copiarDriverAsignacionCentroObjeto(int periodoOrigen, int periodoDestino, int repartoTipo) {
+        String queryStr = String.format("" +
+                "DELETE FROM MS_objeto_driver\n" +
+                " WHERE periodo=%d and reparto_tipo=%d",
+                periodoDestino,repartoTipo);
+        ConexionBD.ejecutar(queryStr);
+        
+        queryStr = String.format("" +
+                "INSERT INTO MS_objeto_driver(centro_codigo,grupo_gasto,driver_codigo,periodo,reparto_tipo,fecha_creacion,fecha_actualizacion)\n" +
+                "SELECT centro_codigo,grupo_gasto,driver_codigo,%d periodo,reparto_tipo,sysdate,sysdate\n" +
+                "  FROM MS_objeto_driver\n" +
+                " WHERE periodo=%d AND reparto_tipo =%d",
+                periodoDestino,
+                periodoOrigen,
+                repartoTipo);
         ConexionBD.ejecutar(queryStr);
     }
     
     public void copiarParametrizacion(int periodoOrigen, int periodoDestino, int repartoTipo) {
         String fechaStr = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date());
         // copiar las Cuentas Contables
-        copiarCuentasContables(periodoOrigen, periodoDestino, fechaStr, repartoTipo);
+        copiarCuentasContables(periodoOrigen, periodoDestino, repartoTipo);
         // copiar las Partidas
-        copiarPartidas(periodoOrigen, periodoDestino, fechaStr, repartoTipo);
+        copiarPartidas(periodoOrigen, periodoDestino, repartoTipo);
         // copiar las Asignaciones de Cuenta - Partida
-        copiarCuentaPartida(periodoOrigen, periodoDestino, fechaStr, repartoTipo);
-//        // copiar los centros de costos
-//        copiarCentroLineas(periodoOrigen, periodoDestino, fechaStr);
-//        // copiar las oficinas
-//        copiarOficinas(periodoOrigen, periodoDestino, fechaStr);
-//        // copiar las bancas
-//        copiarBancas(periodoOrigen, periodoDestino, fechaStr);
-//        // copiar lor productos
-//        copiarProductos(periodoOrigen, periodoDestino, fechaStr);
-//        // copiar los drivers centro lineas
-//        copiarDriverCentroLineas(periodoOrigen, periodoDestino, fechaStr);
-//        // copiar los drivers centro lineas
-//        copiarDriverObjetoLineas(periodoOrigen, periodoDestino, fechaStr);
-//        // copiar los asginacion drivers x entidades
-//        copiarDriverAsignacionEntidades(periodoOrigen, periodoDestino, fechaStr);
+        copiarCuentaPartida(periodoOrigen, periodoDestino, repartoTipo);
+        // copiar los centros de costos
+        copiarCentro(periodoOrigen, periodoDestino, repartoTipo);
+        // copiar lor productos
+        copiarProductos(periodoOrigen, periodoDestino, repartoTipo);
+        // copiar lor subcanales
+        copiarSubcanales(periodoOrigen, periodoDestino, repartoTipo);
+        // copiar los drivers centro lineas
+        copiarDriverCentroLineas(periodoOrigen, periodoDestino, repartoTipo);
+        // copiar los drivers centro lineas
+        copiarDriverObjetoLineas(periodoOrigen, periodoDestino, repartoTipo);
+        // copiar los asginacion drivers x centros staff
+        copiarDriverAsignacionCentroStaff(periodoOrigen, periodoDestino, repartoTipo);
+        // copiar los asginacion drivers x centros Bolsa
+        copiarDriverAsignacionCentroBolsa(periodoOrigen, periodoDestino, repartoTipo);
+        // copiar los asginacion drivers x centros objeto
+        copiarDriverAsignacionCentroObjeto(periodoOrigen, periodoDestino, repartoTipo);
     }
 }
