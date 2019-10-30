65e7aa3eaf4dc58b675289d2e387f71a759ed3a0
Briggette Román <briggette.roman@gmail.com>
2019-10-30 12:43:27 -0500

    Precision

--- src/controlador/procesos/DistribuirFase3Task.java
+++ src/controlador/procesos/DistribuirFase3Task.java
@@ -64,7 +64,7 @@ public class DistribuirFase3Task extends Task {
         for (int i = 1; i <= max; ++i) {
             // inicio logica
             String centroObjetoCodigo = lista.get(i-1);
-            centroDAO.insertarDistribucionCentrosObjetosCosto(centroObjetoCodigo,periodo, principalControlador.menuControlador.repartoTipo);
+            centroDAO.insertarDistribucionCentrosObjetosCosto(centroObjetoCodigo,periodo, principalControlador.menuControlador.repartoTipo, 0.0001);
             principalControlador.piTotal.setProgress(progresoTotal*(fase-1) + i*progresoTotal/(max+1));
             principalControlador.pbTotal.setProgress(progresoTotal*(fase-1) + i*progresoTotal/(max+1));
             // fin logica
